2e5c2d241e2df6aed2fdca6d036c98b829312cc1
tgui9660 <tgui9660@users.noreply.github.com>
2026-06-06 12:41:17 -0400

    Ensure Releases directory exists before building

--- BuildRelease.bat
+++ BuildRelease.bat
@@ -4,7 +4,10 @@ setlocal
 :: Get a robust timestamp via PowerShell (YYYYMMDD_HHmmSS)
 for /f "delims=" %%i in ('powershell -command "Get-Date -Format 'yyyyMMdd_HHmmSS'"') do set TIMESTAMP=%%i
 
-set RELEASE_DIR=%~dp0Releases\Build-%TIMESTAMP%
+set RELEASES_DIR=%~dp0Releases
+if not exist "%RELEASES_DIR%" mkdir "%RELEASES_DIR%"
+
+set RELEASE_DIR=%RELEASES_DIR%\Build-%TIMESTAMP%
 
 :: Publish the release
 dotnet publish WinformsVibes.csproj -c Release -r win-x64 --no-self-contained -o "%RELEASE_DIR%"
